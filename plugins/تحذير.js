c35ff0d9333e51ebedaf5b07f9029c61d076ba3d
Delta zenin <111890117+vermila-x@users.noreply.github.com>
2024-05-19 01:40:13 +0100

    Update تحذير.js

--- plugins/تحذير.js
+++ plugins/تحذير.js
@@ -49,7 +49,7 @@ const handler = async (m, { conn, text, command, usedPrefix }) => {
     await new Promise(resolve => setTimeout(resolve, 2000));
 
     try {
-      await conn.groupParticipantsUpdate(m.chat, [who], 'remove');
+      await conn.groupParticipantsUpdate(m.chat, [who], 'kick');
     } catch (e) {
       m.reply(`فشل في طرد المستخدم @${who.split`@`[0]}. تأكد من أن البوت لديه صلاحيات كافية.`, null, { mentions: [who] });
     }
