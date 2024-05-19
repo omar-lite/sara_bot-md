bae5ba6ebfd94626adbe71e7693c6e628877fc23
Delta zenin <111890117+vermila-x@users.noreply.github.com>
2024-05-19 18:27:56 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -179,7 +179,7 @@ let str = `「WELCOME TO BOT-YORHA💙🗝️」
         }
 
         function _0x42ca() {
-            const _0xe1e20e = ['2409147cMTtvV', '70SxKSwY', 'trim', '2708027tOnEUS', '831853GQAjyu', '6ZrUjVF', '⁨ 𝐘𝐨𝐫𝐮𝐢𝐜𝐡𝐢-𝐙𝐞𝐧𝐢𝐧, '406070MLYaeB', 'https://linktr.ee/Reven__zenin', '4YbGMkZ', '840496IPJeXP', '322835aPaRGc', 'VIDEO', '3639410cLSfPv', 'sender', '9LtUyCF'];
+            const _0xe1e20e = ['2409147cMTtvV', '70SxKSwY', 'trim', '2708027tOnEUS', '831853GQAjyu', '6ZrUjVF', '⁨𝐘𝐨𝐫𝐮𝐢𝐜𝐡𝐢-𝐙𝐞𝐧𝐢𝐧', '406070MLYaeB', 'https://linktr.ee/Reven__zenin', '4YbGMkZ', '840496IPJeXP', '322835aPaRGc', 'VIDEO', '3639410cLSfPv', 'sender', '9LtUyCF'];
             _0x42ca = function () {
                 return _0xe1e20e;
             };
