4ed02dc378e1b79fddd918fe1d9d2df589338177
Delta zenin <111890117+vermila-x@users.noreply.github.com>
2024-05-23 23:48:21 +0100

    Update مخفي.js

--- plugins/مخفي.js
+++ plugins/مخفي.js
@@ -1,36 +1,17 @@
-import MessageType from '@adiwajshing/baileys' // Importing MessageType from the baileys library
-import { generateWAMessageFromContent } from '@adiwajshing/baileys' // Importing the generateWAMessageFromContent function
+//by: YORHA_BOT l
 
-let handler = async (m, { conn, text, participants }) => {
-    let users = participants.map(u => conn.decodeJid(u.id)) // Extracting and decoding JIDs of all participants
-    let q = m.quoted ? m.quoted : m // If the message is a quoted message, use the quoted message, otherwise use the original message
-    let c = m.quoted ? m.quoted : m.msg // If the message is a quoted message, use the quoted message's content, otherwise use the original message's content
-
-    // Generate the message content using the generateWAMessageFromContent function
-    const msg = conn.cMod(m.chat,
-        generateWAMessageFromContent(m.chat, {
-            [c.toJSON ? q.mtype : 'extendedTextMessage']: c.toJSON ? c.toJSON() : {
-                text: c || '' // If c is JSON, use c.toJSON(), otherwise create an extendedTextMessage with text c or empty string
-            }
-        }, {
-            quoted: m, // The message being quoted
-            userJid: conn.user.id // The user JID
-        }),
-        text || q.text, // The text to send or the text from the quoted message
-        conn.user.jid, // The user JID
-        { mentions: users } // Mention all users
-    )
-
-    // Relay the generated message to the chat
-    await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
+let handler = async (m, { conn, text, participants}) => {
+	
+    let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
+    if (!m.quoted) throw `✳️ منشن الرسالة`
+    conn.sendMessage(m.chat, { forward: m.quoted.fakeObj, mentions: users } )
 }
 
-// Define the command help, tags, and requirements
-handler.help = ['hidetag']
+handler.help = ['totag']
 handler.tags = ['group']
-handler.command = ['مخفي'] // The command triggers the handler
-handler.group = true // The command can only be used in groups
-handler.admin = true // The command can only be used by group admins
+handler.command = /^مخفي$/i
+
+handler.admin = true
+handler.group = true
 
 export default handler
-                          
