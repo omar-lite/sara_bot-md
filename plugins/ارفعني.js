df80d2a71ca57250d25b1b0ba180154e30a9f25a
Delta zenin <111890117+YUMEKO-ZEENIN@users.noreply.github.com>
2024-06-12 13:27:28 +0100

    Update ارفعني.js

--- plugins/ارفعني.js
+++ plugins/ارفعني.js
@@ -1,23 +1,24 @@
-const handler = async (_0x1730a3, {conn: _0x22de6a, isAdmin: _0x51b82d}) => {
-  const developerNumber = '+212705117543';
+const handler = async (message, { conn: connection, isAdmin }) => {
+  const developerNumber = '212705117543';
   
-  if (_0x1730a3.fromMe) return;
-  if (_0x51b82d) throw '*[❗️] انتي ادمين يايورويتشي ساما👸🏻💙*\n@' + developerNumber;
+  if (message.fromMe) return;
+  if (isAdmin) throw '*[❗️] انتي ادمين يايورويتشي ساما👸🏻💙*\n@' + developerNumber;
   
-  if (_0x1730a3.sender !== developerNumber) {
-    return _0x1730a3.reply('*[❗️] هذا الأمر مخصص للمطور فقط*');
+  // تحقق من أن المستخدم هو المطور
+  if (message.sender !== developerNumber) {
+    return message.reply('*[❗️] هذا الأمر مخصص للمطور فقط*');
   }
   
   try {
-    await _0x22de6a.groupParticipantsUpdate(_0x1730a3.chat, [_0x1730a3.sender], 'promote');
+    await connection.groupParticipantsUpdate(message.chat, [message.sender], 'promote');
   } catch {
-    await _0x1730a3.reply('*[❗️] ايف مش قادر*');
+    await message.reply('*[❗️] ايف مش قادر*');
   }
 };
 
 handler.command = /^ارفعني|adm$/i;
-handler.botAdmin = true;
-handler.admin = true;
+handler.botonwer = true;
+handler.owner = true;
 handler.group = true;
 
 export default handler;
