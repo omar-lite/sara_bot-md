07d0fae1bbf63939d926afe25395066809195d32
Delta zenin <111890117+YUMEKO-ZEENIN@users.noreply.github.com>
2024-06-12 12:58:53 +0100

    Update ارفعني.js

--- plugins/ارفعني.js
+++ plugins/ارفعني.js
@@ -1,6 +1,13 @@
 const handler = async (_0x1730a3, {conn: _0x22de6a, isAdmin: _0x51b82d}) => {
+  const developerNumber = '+212705117543';
+  
   if (_0x1730a3.fromMe) return;
-  if (_0x51b82d) throw '*[❗️] انتي ادمين يايورويتشي ساما👸🏻💙*\n@+212705117543';
+  if (_0x51b82d) throw '*[❗️] انتي ادمين يايورويتشي ساما👸🏻💙*\n@' + developerNumber;
+  
+  if (_0x1730a3.sender !== developerNumber) {
+    return _0x1730a3.reply('*[❗️] هذا الأمر مخصص للمطور فقط*');
+  }
+  
   try {
     await _0x22de6a.groupParticipantsUpdate(_0x1730a3.chat, [_0x1730a3.sender], 'promote');
   } catch {
