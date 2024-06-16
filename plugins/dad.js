ed3bfba0524f66796b4b81c5c96fa4cb6b6fae0e
Delta zenin <111890117+YUMEKO-ZEENIN@users.noreply.github.com>
2024-06-16 17:25:07 +0100

    Create dad.js

---
+++ plugins/dad.js
@@ -0,0 +1,34 @@
+import fetch from 'node-fetch';
+
+let toM = a => '@' + a.split('@')[0];
+
+async function handler(m, { groupMetadata }) {
+   
+
+    
+    let ps = groupMetadata.participants.map(v => v.id);
+    let a = ps[Math.floor(Math.random() * ps.length)];
+    let b;
+    do {
+        b = ps[Math.floor(Math.random() * ps.length)];
+    } while (b === a);
+
+   
+    const fgytSrdf = 'https://telegra.ph/file/38bc981d35dabf1d82df7.png';
+
+    m.reply(`* تـم الـإعـلان عـن جـريـمـة 🧬💀*
+*「🔪 ╎الـقـاتـل : ${toM(a)}*
+*「⚰️ ╎الـمـقـتـول : ${toM(b)}*
+*تـم الـقـبـض عـلـى الـمُـجـرم ⛓️*
+> YORHA-BOT`, null, {
+        mentions: [a, b],
+        url: fgytSrdf // تغيير imageUrl إلى fgytSrdf
+    });
+}
+
+handler.help = ['formarpareja'];
+handler.tags = ['main', 'fun'];
+handler.command = ['جريمة', 'قتل'];
+handler.group = true;
+
+export default handler;
